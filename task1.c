5c56c154c000829243b8a3b439b181e1ae43952b
abubakarpandu <108415686+abubakarpandu@users.noreply.github.com>
2022-06-29 20:16:39 +0300

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void exchange(int *p,int *q);
+
+int main(){
+int c,d;
+printf("enter c and d\n");
+scanf("%d%d",&c,&d);
+exchange(&c,&d);
+printf("c=%d and d=%d",c,d);
+return 0;
+}
+void exchange(int *p,int *q){
+	int temp;
+	temp=*p;
+	*p=*q;
+	*q=temp;
+}
+
